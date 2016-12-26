2757200b763bd4f4d1b9559a693608266461a5b2
Sergey Masuryan <smasuryan@plgn.ru>
2016-12-26 10:40:43 +0500

    date time server

--- client.cpp
+++ client.cpp
@@ -16,32 +16,31 @@ const int port = 3425;
 
 int main()
 {
-    int sTime = 0; /* дискриптор сокета */
-    time_t currentTime  = time(NULL);;
-    struct sockaddr_in addr;
+      int sTime = 0; /* дискриптор сокета */
+      time_t currentTime  = time(NULL);;
+      struct sockaddr_in addr;
 
-    sTime = socket(AF_INET, SOCK_STREAM, 0);
-    if(sTime < 0) {
-        perror("socket");
-        return -1;
-        }
+      sTime = socket(AF_INET, SOCK_STREAM, 0);
+      if(sTime < 0) {
+              perror("socket");
+              return -1;
+          }
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    //inet_pton(AF_INET, "172.16.5.110", &addr.sin_addr); /* local test*/
-    inet_pton(AF_INET, "89.223.26.96", &addr.sin_addr);    /* internet test */
+      addr.sin_family = AF_INET;
+      addr.sin_port = htons(port);
+      inet_pton(AF_INET, "172.16.5.110", &addr.sin_addr); /* local test*/
+      //inet_pton(AF_INET, "89.223.26.96", &addr.sin_addr);    /* internet test */
 
-    /* проверка соединения */
-    if(connect(sTime, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        perror("connect");
-        return -2;
-        }
+      /* проверка соединения */
+      if(connect(sTime, (sockaddr *)&addr, sizeof(addr)) < 0) {
+              perror("connect");
+              return -2;
+          }
 
-    recv(sTime, &currentTime, sizeof(currentTime), 0); /* получаем сооьщение*/
-    cout << ctime(&currentTime);
+      recv(sTime, &currentTime, sizeof(currentTime), 0); /* получаем сооьщение*/
+      cout << ctime(&currentTime);
 
-    close(sTime); /* закрываем дискриптор сокета */
+      close(sTime); /* закрываем дискриптор сокета */
 
-
-    return 0;
+      return 0;
 }
